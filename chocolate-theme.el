e797d2fe57bd3f57dc9ad0a17741068bb8bd04df
Valeriy Savchenko <sinmipt@gmail.com>
2018-04-22 15:18:34 +0300

    Add autoload.

--- chocolate-theme.el
+++ chocolate-theme.el
@@ -1503,6 +1503,14 @@
    (:foreground chocolate-hue-1))
   ))
 
+
+;;;###autoload
+(and load-file-name
+     (boundp 'custom-theme-load-path)
+     (add-to-list 'custom-theme-load-path
+                  (file-name-as-directory
+                   (file-name-directory load-file-name))))
+
 (provide-theme 'chocolate)
 
 ;;; emacs-chocolate-theme.el ends here
