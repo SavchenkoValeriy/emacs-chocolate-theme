5cef07723f0159d23bc3fc09acf234af8940e100
Valeriy Savchenko <sinmipt@gmail.com>
2019-07-22 01:36:39 +0300

    Remove redundant code

--- chocolate-theme.el
+++ chocolate-theme.el
@@ -1638,7 +1638,6 @@
 
 ;;;###autoload
 (and load-file-name
-     (boundp 'custom-theme-load-path)
      (add-to-list 'custom-theme-load-path
                   (file-name-as-directory
                    (file-name-directory load-file-name))))
